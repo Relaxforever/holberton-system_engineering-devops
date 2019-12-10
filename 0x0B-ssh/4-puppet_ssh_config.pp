b388256024ce86ff4413306971d3ce39428eeb73
Fidel Caicedo <869@holbertonschool.com>
2019-12-10 10:02:22 -0500

    uploading correct name for fourth point

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 #This file creates a holberton file in /tmp/holberton
-line { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   }
-line { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
