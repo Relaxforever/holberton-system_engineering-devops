fbf09b33441ff60aff5e58177a8d6e17d5289631
Fidel Caicedo <869@holbertonschool.com>
2020-02-03 22:10:45 -0500

    uploading readme and zero point

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# get the info of a worker
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    user_name = get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(argv[1]))
+    todo_user = get("https://jsonplaceholder.typicode.com/todos")
+    user_json = user_name.json()
+    list_json = todo_user.json()
+    all_task = 0
+    done_task = 0
+    string_task = ""
+
+    for dict_data in list_json:
+        if dict_data.get('userId') == int(argv[1]):
+            if dict_data.get('completed'):
+                done_task += 1
+                string_task += "\t " + dict_data.get('title') + "\n"
+            all_task += 1
+    print("Employee {} is done with task ({}/{})".format(user_json.get('name'),
+                                                         done_task,
+                                                         all_task))
+    print(string_task,end='')
