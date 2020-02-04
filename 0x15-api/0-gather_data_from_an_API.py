69775388fd843a95d55c8a7004a487ed4a760243
Fidel Caicedo <869@holbertonschool.com>
2020-02-03 22:49:22 -0500

    fixing typo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,7 @@ from sys import argv
 if __name__ == "__main__":
     user_name = get("https://jsonplaceholder.typicode.com/users/{}"
                     .format(argv[1]))
-    todo_user = get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                    .format(argv[1]))
+    todo_user = get("https://jsonplaceholder.typicode.com/todos")
     user_json = user_name.json()
     list_json = todo_user.json()
     all_task = 0
@@ -15,11 +14,12 @@ if __name__ == "__main__":
     string_task = []
 
     for dict_data in list_json:
-        if dict_data.get('completed'):
-            done_task += 1
-            string_task.append(dict_data.get('title'))
-        all_task += 1
-    print("Employee {} is done with task ({}/{})"
+        if dict_data.get('userId') == int(argv[1]):
+            if dict_data.get('completed'):
+                done_task += 1
+                string_task.append(dict_data.get('title'))
+            all_task += 1
+    print("Employee {} is done with task ({}/{}):"
           .format(user_json.get('name'), done_task, all_task))
     for string_title in string_task:
         print('\t {}'.format(string_title))
