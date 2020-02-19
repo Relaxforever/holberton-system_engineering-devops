46fc32cf5368658451f97055c97ee096e57fb247
Relaxforever <869@holbertonschool.com>
2020-02-19 11:25:24 -0500

    adding , so it parses correctly

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes a wrong call to a unknown file
 exec { 'Replace Bad instances':
-command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 path    => ['/bin', '/usr/bin']
 }
